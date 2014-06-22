1741364d745e30f9c0a1f9a8b3ee22d2152998d4
luisdza <luisd@syeop.co.za>
2014-06-22 17:14:03 +0200

    * Updated Java Bean fields

--- APSEngine/src/main/java/elen7045/group5/project/aps/billing/MunicipalityStmntBean.java
+++ APSEngine/src/main/java/elen7045/group5/project/aps/billing/MunicipalityStmntBean.java
@@ -6,6 +6,68 @@ package elen7045.group5.project.aps.billing;
 public class MunicipalityStmntBean extends StatementBean
 {
 	private static final long serialVersionUID = 122222L;
+	private String instalmentNotice;
+	private float	electricityUsed,
+					electricityCharges,
+					gasUsed,
+					gasCharges,
+					waterUsed,
+					waterCharges,
+					sewerageCharges,
+					refuseCharges;
 	
-	//TODO: implement
+	public String getInstalmentNotice() {
+		return instalmentNotice;
+	}
+	public void setInstalmentNotice(String instalmentNotice) {
+		this.instalmentNotice = instalmentNotice;
+	}
+	public float getElectricityUsed() {
+		return electricityUsed;
+	}
+	public void setElectricityUsed(float electricityUsed) {
+		this.electricityUsed = electricityUsed;
+	}
+	public float getElectricityCharges() {
+		return electricityCharges;
+	}
+	public void setElectricityCharges(float electricityCharges) {
+		this.electricityCharges = electricityCharges;
+	}
+	public float getGasUsed() {
+		return gasUsed;
+	}
+	public void setGasUsed(float gasUsed) {
+		this.gasUsed = gasUsed;
+	}
+	public float getGasCharges() {
+		return gasCharges;
+	}
+	public void setGasCharges(float gasCharges) {
+		this.gasCharges = gasCharges;
+	}
+	public float getWaterUsed() {
+		return waterUsed;
+	}
+	public void setWaterUsed(float waterUsed) {
+		this.waterUsed = waterUsed;
+	}
+	public float getWaterCharges() {
+		return waterCharges;
+	}
+	public void setWaterCharges(float waterCharges) {
+		this.waterCharges = waterCharges;
+	}
+	public float getSewerageCharges() {
+		return sewerageCharges;
+	}
+	public void setSewerageCharges(float sewerageCharges) {
+		this.sewerageCharges = sewerageCharges;
+	}
+	public float getRefuseCharges() {
+		return refuseCharges;
+	}
+	public void setRefuseCharges(float refuseCharges) {
+		this.refuseCharges = refuseCharges;
+	}
 }
--- APSEngine/src/main/java/elen7045/group5/project/aps/billing/TelcoStmntBean.java
+++ APSEngine/src/main/java/elen7045/group5/project/aps/billing/TelcoStmntBean.java
@@ -6,6 +6,40 @@ package elen7045.group5.project.aps.billing;
 public class TelcoStmntBean extends StatementBean
 {
 	private static final long serialVersionUID = 122222L;
+	private String telephoneNumber;
+	private int totalNumberOfCalls,
+				totalCallDurationSeconds;
+	private float serviceCharges,
+					callCharges;
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	public int getTotalNumberOfCalls() {
+		return totalNumberOfCalls;
+	}
+	public void setTotalNumberOfCalls(int totalNumberOfCalls) {
+		this.totalNumberOfCalls = totalNumberOfCalls;
+	}
+	public int getTotalCallDurationSeconds() {
+		return totalCallDurationSeconds;
+	}
+	public void setTotalCallDurationSeconds(int totalCallDurationSeconds) {
+		this.totalCallDurationSeconds = totalCallDurationSeconds;
+	}
+	public float getServiceCharges() {
+		return serviceCharges;
+	}
+	public void setServiceCharges(float serviceCharges) {
+		this.serviceCharges = serviceCharges;
+	}
+	public float getCallCharges() {
+		return callCharges;
+	}
+	public void setCallCharges(float callCharges) {
+		this.callCharges = callCharges;
+	}
 	
-	//TODO: implement
 }
