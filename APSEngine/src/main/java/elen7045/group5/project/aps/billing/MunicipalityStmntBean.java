f30ec6a6ec536bf8b08cd1da0a52d06ff9db4afe
luisdza <luisd@syeop.co.za>
2014-06-23 22:29:10 +0200

    Added comments to beans

--- APSEngine/src/main/java/elen7045/group5/project/aps/billing/MunicipalityStmntBean.java
+++ APSEngine/src/main/java/elen7045/group5/project/aps/billing/MunicipalityStmntBean.java
@@ -16,91 +16,154 @@ public class MunicipalityStmntBean extends StatementBean
 								sewerageCharges,
 								refuseCharges;
 
+	/**
+	 * @return Returns the instalmentNotice
+	 */
 	public String getInstalmentNotice()
 	{
 		return instalmentNotice;
 	}
 
+	/**
+	 * @param instalmentNotice
+	 *             - The instalmentNotice to set
+	 */
 	public void setInstalmentNotice(String instalmentNotice)
 	{
 		this.instalmentNotice = instalmentNotice;
 	}
 
+	/**
+	 * @return  Returns the electricityUsed
+	 */
 	public float getElectricityUsed()
 	{
 		return electricityUsed;
 	}
 
+	/**
+	 * @param  electricityUsed
+	 *             - The electricityUsed to set
+	 */
 	public void setElectricityUsed(float electricityUsed)
 	{
 		this.electricityUsed = electricityUsed;
 	}
 
+	/**
+	 * @return  Returns the electricityCharges
+	 */
 	public float getElectricityCharges()
 	{
 		return electricityCharges;
 	}
 
+	/**
+	 * @param electricityCharges
+	 *             - The electricityCharges to set
+	 */
 	public void setElectricityCharges(float electricityCharges)
 	{
 		this.electricityCharges = electricityCharges;
 	}
 
+	/**
+	 * @return  Returns the gasUsed
+	 */
 	public float getGasUsed()
 	{
 		return gasUsed;
 	}
 
+	/**
+	 * @param gasUsed
+	 *             - The gasUsed to set
+	 */
 	public void setGasUsed(float gasUsed)
 	{
 		this.gasUsed = gasUsed;
 	}
 
+	/**
+	 * @return  Returns the gasCharges
+	 */
 	public float getGasCharges()
 	{
 		return gasCharges;
 	}
 
+	/**
+	 * @param gasCharges
+	 *             - The gasCharges to set
+	 */
 	public void setGasCharges(float gasCharges)
 	{
 		this.gasCharges = gasCharges;
 	}
 
+	/**
+	 * @return  Returns the waterUsed
+	 */
 	public float getWaterUsed()
 	{
 		return waterUsed;
 	}
 
+	/**
+	 * @param waterUsed
+	 *             - The waterUsed to set
+	 */
 	public void setWaterUsed(float waterUsed)
 	{
 		this.waterUsed = waterUsed;
 	}
 
+	/**
+	 * @return  Returns the waterCharges
+	 */
 	public float getWaterCharges()
 	{
 		return waterCharges;
 	}
 
+	/**
+	 * @param waterCharges
+	 *             - The waterCharges to set
+	 */
 	public void setWaterCharges(float waterCharges)
 	{
 		this.waterCharges = waterCharges;
 	}
 
+	/**
+	 * @return  Returns the sewerageCharges
+	 */
 	public float getSewerageCharges()
 	{
 		return sewerageCharges;
 	}
 
+	/**
+	 * @param sewerageCharges
+	 *             - The sewerageCharges to set
+	 */
 	public void setSewerageCharges(float sewerageCharges)
 	{
 		this.sewerageCharges = sewerageCharges;
 	}
 
+	/**
+	 * @return  Returns the refuseCharges
+	 */
 	public float getRefuseCharges()
 	{
 		return refuseCharges;
 	}
 
+	/**
+	 * @param refuseCharges
+	 *             - The refuseCharges to set
+	 */
 	public void setRefuseCharges(float refuseCharges)
 	{
 		this.refuseCharges = refuseCharges;
