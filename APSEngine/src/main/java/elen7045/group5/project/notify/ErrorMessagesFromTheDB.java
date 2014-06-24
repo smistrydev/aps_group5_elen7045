1d1a56b015343e2671b8f227ce68c41318d6b0da
mulaudzitn <mulaudzitn@gmail.com>
2014-06-24 19:47:19 +0200

    use the repository to get the error description

--- APSEngine/src/main/java/elen7045/group5/project/notify/ErrorMessagesFromTheDB.java
+++ APSEngine/src/main/java/elen7045/group5/project/notify/ErrorMessagesFromTheDB.java
@@ -1,8 +1,13 @@
 package elen7045.group5.project.notify;
 
+import elen7045.group5.project.aps.jpa.model.Notification;
+import elen7045.group5.project.aps.jpa.service.NotificationService;
+
 public class ErrorMessagesFromTheDB {
 	 private String errorMessageFromDB;
 	 private int errorCode;
+	 private NotificationService service;
+	 private Notification notification;
 	 
 	    public ErrorMessagesFromTheDB(int errorCode)
 	    {
@@ -16,6 +21,9 @@ public class ErrorMessagesFromTheDB {
 	    
 	    private  void fetchErrorMessageFromDB()
 	    {
+	    	Integer error = new Integer(errorCode);
+	    	notification =	service.findById(error);
+	    	errorMessageFromDB = notification.getNotificationDescription();
 	    //	errorMessageFromDB =
 	    }
 	    
