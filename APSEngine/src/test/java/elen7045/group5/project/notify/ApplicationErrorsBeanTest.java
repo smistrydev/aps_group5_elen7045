8facba2eabc69c018eb4074cc7cff9e9ace380ef
mulaudzitn <mulaudzitn@gmail.com>
2014-06-28 16:59:11 +0200

    Add Test for the Application errors bean

---
+++ APSEngine/src/test/java/elen7045/group5/project/notify/ApplicationErrorsBeanTest.java
@@ -0,0 +1,42 @@
+package elen7045.group5.project.notify;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationErrorsBeanTest {
+	ApplicationErrorsBean app1 = new ApplicationErrorsBean(EApplicationErrors.ACTION_REQUIRED_BY_BILLING_WEBSITE);
+	ApplicationErrorsBean app2 = new ApplicationErrorsBean(EDataErrors.DATA_FAILS_INTEGRITY_CHECKING);
+	ApplicationErrorsBean app3 = new ApplicationErrorsBean(EScrapperSchedularErrors.SCHEDUKER_REACHED_MAXIMUM_RETRIES);
+	ApplicationErrorsBean app4 = new ApplicationErrorsBean(EUserInterfaceErrors.INVALID_USER_NAME);
+	@Test
+	public void testApplicationErrorsBeanEApplicationErrors() {
+		assertEquals(2004, app1.getErrorCode());
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplicationErrorsBeanEDataErrors() {
+	//	fail("Not yet implemented");
+		assertEquals(1005, app2.getErrorCode());
+	}
+
+	@Test
+	public void testApplicationErrorsBeanEScrapperSchedularErrors() {
+	//	fail("Not yet implemented");
+		assertEquals(3002, app3.getErrorCode());
+	}
+
+	@Test
+	public void testApplicationErrorsBeanEUserInterfaceErrors() {
+		//fail("Not yet implemented");
+		assertEquals(001, app4.getErrorCode());
+	}
+
+	@Test
+	
+	public void testGetErrorCode() {
+		assertEquals(3002, app3.getErrorCode());
+	}
+
+}
