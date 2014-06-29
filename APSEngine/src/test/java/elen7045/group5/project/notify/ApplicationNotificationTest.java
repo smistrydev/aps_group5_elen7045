7d0ff737c17dbb43ff2ee405c0b103bf6ba3572d
Sanjay Mistry <smistrydev@users.noreply.github.com>
2014-06-29 16:30:45 +0200

    * changed dbscript and added errormessage - notification test. Done by Norman.

--- APSEngine/src/test/java/elen7045/group5/project/notify/ApplicationNotificationTest.java
+++ APSEngine/src/test/java/elen7045/group5/project/notify/ApplicationNotificationTest.java
@@ -74,33 +74,64 @@ public class ApplicationNotificationTest
 	 * Test method for
 	 * {@link elen7045.group5.project.notify.service.ApplicationNotificationServiceImpl#xmlNotificationMessage(elen7045.group5.project.notify.EUserInterfaceErrors)}
 	 * .
+	 * 
+	 * @throws JAXBException
 	 */
-	// @Test
-	public void testXmlNotificationMessageEUserInterfaceErrors()
+	@Test
+	public void testXmlNotificationMessageEUserInterfaceErrors() throws JAXBException
 	{
-		fail("Not yet implemented");
+		EUserInterfaceErrors userInterfaceErrors = EUserInterfaceErrors.INVALID_USER_NAME;
+		String xml = applicationNotificationService.notificationMessageToXML(userInterfaceErrors);
+
+		assertNotNull(xml);
+
+		NotificationMessage message = NotificationXMLUtil.fromXML(xml);
+
+		assertEquals("0001", message.getNotificationCode());
+		assertEquals("INVALID_USER_NAME", message.getNotificationMessage());
+
 	}
 
 	/**
 	 * Test method for
 	 * {@link elen7045.group5.project.notify.service.ApplicationNotificationServiceImpl#xmlNotificationMessage(elen7045.group5.project.notify.EScrapperSchedularErrors)}
 	 * .
+	 * @throws JAXBException 
 	 */
-	// @Test
-	public void testXmlNotificationMessageEScrapperSchedularErrors()
+	@Test
+	public void testXmlNotificationMessageEScrapperSchedularErrors() throws JAXBException
 	{
-		fail("Not yet implemented");
+		EScrapperSchedularErrors scrapperSchedularErrors = EScrapperSchedularErrors.SCHEDULER_REACHED_MAXIMUM_RETRIES;
+		String xml = applicationNotificationService.notificationMessageToXML(scrapperSchedularErrors);
+
+		assertNotNull(xml);
+		
+		NotificationMessage message = NotificationXMLUtil.fromXML(xml);
+		
+		assertEquals("3002", message.getNotificationCode());
+		assertEquals("SCHEDULER_REACHED_MAXIMUM_RETRIES", message.getNotificationMessage());
+
 	}
 
 	/**
 	 * Test method for
 	 * {@link elen7045.group5.project.notify.service.ApplicationNotificationServiceImpl#xmlNotificationMessage(elen7045.group5.project.notify.EDataErrors)}
 	 * .
+	 * @throws JAXBException 
 	 */
-	// @Test
-	public void testXmlNotificationMessageEDataErrors()
+	@Test
+	public void testXmlNotificationMessageEDataErrors() throws JAXBException
 	{
-		fail("Not yet implemented");
+		EDataErrors dataErrors = EDataErrors.DATA_FAILS_INTEGRITY_CHECKING;
+		String xml = applicationNotificationService.notificationMessageToXML(dataErrors);
+
+		assertNotNull(xml);
+		
+		NotificationMessage message = NotificationXMLUtil.fromXML(xml);
+		
+		assertEquals("1005", message.getNotificationCode());
+		assertEquals("DATA_FAILS_INTEGRITY_CHECKING", message.getNotificationMessage());
+
 	}
 
 }
