1f951de33b5efb93505fa0b8a053e040791842cf
Sanjay Mistry <smistrydev@users.noreply.github.com>
2014-07-03 20:26:51 +0200

    * test added for encryption

--- APSEngine/src/test/java/elen7045/group5/project/aps/encrypt/service/PasswordAPSEncryptorTest.java
+++ APSEngine/src/test/java/elen7045/group5/project/aps/encrypt/service/PasswordAPSEncryptorTest.java
@@ -2,7 +2,7 @@ package elen7045.group5.project.aps.encrypt.service;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.junit.Test;
@@ -29,11 +29,51 @@ public class PasswordAPSEncryptorTest
 	public void testCheck()
 	{
 		PasswordAPSEncryptor apsEncryptor = new PasswordAPSEncryptor();
-
 		String password = "Th#sIs@Fu&nyPass01";
+
 		String encryptedPass = apsEncryptor.encrypt(password);
 		assertTrue(apsEncryptor.check(password, encryptedPass));
 
+	}
+
+	// Feature 1: Customer Password one way encryption
+	@Test
+	public void testOneWayEncryption()
+	{
+		String password = "SecureP@s5";
+		APSEncryptor apsEncryptor = new PasswordAPSEncryptor();
+		String encryptedPass = apsEncryptor.encrypt(password);
+
+		assertNotNull(encryptedPass);
+		assertNotEquals(password, encryptedPass);
+	}
+
+	// Feature 2: Customer Password verification when signing in APS
+	@Test
+	public void testVerifyPassword()
+	{
+		String password = "SecureP@s5";
+		APSEncryptor apsEncryptor = new PasswordAPSEncryptor();
+		String originalEncryptedPass = apsEncryptor.encrypt(password);
+
+		String password2 = "SecureP@s5";
+		boolean result = apsEncryptor.check(password2, originalEncryptedPass);
+
+		assertTrue(result);
+	}
+
+	// Feature 3: Customer Password cannot be decrypted
+	@Test
+	public void testFailAttemptToDecryptPassword()
+	{
+		String password = "SecureP@s5";
+		APSEncryptor apsEncryptor = new PasswordAPSEncryptor();
+		String originalEncryptedPass = apsEncryptor.encrypt(password);
+
+		String attemptUnEncrypt = apsEncryptor.encrypt(originalEncryptedPass);
+		boolean result = apsEncryptor.check(attemptUnEncrypt, originalEncryptedPass);
+
+		assertFalse(result);
 	}
 
 }
